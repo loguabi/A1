546d53022955f4e2acc0e05cc13272ec58f39498
logitha <loga848.456@gmail.com>
2016-07-05 15:41:19 +0530

    Create A1.c

---
+++ A1.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b=0,i;
+printf("Enter the value:");
+scanf("%d",&a);
+for(a=0;a<=i;a++)
+{
+if(a%2!=0)
+{
+b=b+a;
+
+}
+printf("%d",b);
+}
+getch();
+}
